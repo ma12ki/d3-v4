c0ede8ac97f062864359b3c1243dfa925bc5b1ea
Marcin Zolkiewski <Marcin_Zolkiewski@epam.com>
2016-11-06 19:12:25 +0100

    selecting DOM elements

--- src/app.js
+++ src/app.js
@@ -1,15 +1,2 @@
-d3.json('data/data.json', (data) => {
-    const extent = d3.extent(data, (row) => {
-        return row.age;
-    });
-
-    const scale = d3.scaleLinear()
-        .domain(extent)
-        .range([0, 600]);
-
-    const ages = d3.set(data, (row) => {
-        return row.age;
-    });
-
-    console.log(ages.values());
-});
+const menu = d3.select('ul.menu');
+console.log(menu.nodes());
