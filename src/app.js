a9fc1f63380a65c7aae09e3e3fab516344bef853
Marcin Zolkiewski <Marcin_Zolkiewski@epam.com>
2016-11-06 17:16:13 +0100

    time scales

--- src/app.js
+++ src/app.js
@@ -10,3 +10,14 @@ console.log(linearScale(30));
 console.log(linearScale(3000));
 
 console.log(linearScale.invert(333)); // translate from output range back to input range
+
+/////////////////////////////////
+
+const timeScale = d3.scaleTime()
+    .domain([new Date(2016, 0 , 1), new Date()])
+    .range([0, 100]);
+
+console.log(timeScale(new Date(2016, 2, 22)));
+console.log(timeScale(new Date(2016, 4, 4)));
+
+console.log(timeScale.invert(50));
