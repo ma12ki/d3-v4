439e93d743dcbe2414406bde13261c09f7e93541
Marcin Zolkiewski <Marcin_Zolkiewski@epam.com>
2016-11-08 22:09:52 +0100

    simple data visualization

--- src/app.js
+++ src/app.js
@@ -1,2 +1,29 @@
-const menu = d3.select('ul.menu');
-console.log(menu.nodes());
+const scores = [
+    { name: 'Alice', score: 96 },
+    { name: 'Billy', score: 83 },
+    { name: 'Cindy', score: 91 },
+    { name: 'David', score: 96 },
+    { name: 'Emily', score: 88 }
+];
+
+const update = d3.select('.chart')
+    .selectAll('div')
+    .data(scores, function(d) {
+        return d ? d.name : this.innerText;
+    })
+    .style('color', 'blue');
+
+const enter = update.enter()
+    .append('div')
+    .text((d) => {
+        return d.name;
+    })
+    .style('color', 'green');
+
+update.exit().remove();
+
+update.merge(enter)
+    .style('width', d => `${d.score}px`)
+    .style('height', '50px')
+    .style('background', 'lightgreen')
+    .style('border', '1px solid black');
