39daad11c4d215b46be01c3e9a6cda21ff3d8e2f
Marcin Zolkiewski <Marcin_Zolkiewski@epam.com>
2016-11-08 23:03:42 +0100

    selection.call

--- src/app.js
+++ src/app.js
@@ -18,10 +18,40 @@ const bar = d3.select('.chart')
 
 bar.append('rect')
     .style('width', d => `${d.score}px`)
-    .classed('bar', true);
+    .classed('bar', true)
+    .on('mouseover', function(d, i, elements) {
+        d3.select(this)
+            .call(scaleBar, 2)
+            .call(setFill, 'orange');
+
+        d3.selectAll(elements)
+            .filter(':not(:hover)')
+            .call(fade, 0.5);
+    })
+    .on('mouseout', function(d, i, elements) {
+        d3.select(this)
+            .call(scaleBar, 1)
+            .call(setFill, 'lightgreen');
+
+        d3.selectAll(elements)
+            .call(fade, 1);
+    });
 
 bar.append('text')
     .attr('y', 20)
     .text((d) => {
         return d.name;
     });
+
+
+function scaleBar(selection, scale) {
+    selection.style('transform', `scaleX(${scale})`);
+}
+
+function fade(selection, opacity) {
+    selection.style('fill-opacity', opacity);
+}
+
+function setFill(selection, color) {
+    selection.style('fill', color);
+}
