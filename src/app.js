dfc80d57c48e84af896f7d32b9ed38d4a614d142
Marcin Zolkiewski <Marcin_Zolkiewski@epam.com>
2016-11-09 22:51:26 +0100

    margin convention

--- src/app.js
+++ src/app.js
@@ -1,57 +1,31 @@
-const scores = [
-    { name: 'Alice', score: 96 },
-    { name: 'Billy', score: 83 },
-    { name: 'Cindy', score: 91 },
-    { name: 'David', score: 96 },
-    { name: 'Emily', score: 88 }
-];
+const margin = {
+    top: 10,
+    right: 20,
+    bottom: 25,
+    left: 25
+};
 
-const bar = d3.select('.chart')
+const chartWidth = 425;
+const chartHeight = 625;
+const width = chartWidth - margin.left - margin.right;
+const height = chartHeight - margin.top - margin.bottom;
+
+const svg = d3.select('.chart')
     .append('svg')
-        .attr('width', 225)
-        .attr('height', 300)
-    .selectAll('rect')
-    .data(scores)
-    .enter()
-        .append('g')
-        .attr('transform', (d, i) => `translate(0, ${i*33})`);
+        .attr('width', chartWidth)
+        .attr('height', chartHeight)
+    .append('g')
+        .attr('transform', `translate(${margin.left}, ${margin.top})`);
 
-bar.append('rect')
-    .style('width', d => `${d.score}px`)
-    .classed('bar', true)
-    .on('mouseover', function(d, i, elements) {
-        d3.select(this)
-            .call(scaleBar, 2)
-            .call(setFill, 'orange');
+svg.append('rect')
+    .attr('width', width / 2)
+    .attr('height', height)
+    .style('fill', 'lightblue')
+    .style('stroke', 'green');
 
-        d3.selectAll(elements)
-            .filter(':not(:hover)')
-            .call(fade, 0.5);
-    })
-    .on('mouseout', function(d, i, elements) {
-        d3.select(this)
-            .call(scaleBar, 1)
-            .call(setFill, 'lightgreen');
-
-        d3.selectAll(elements)
-            .call(fade, 1);
-    });
-
-bar.append('text')
-    .attr('y', 20)
-    .text((d) => {
-        return d.name;
-    });
-
-
-function scaleBar(selection, scale) {
-    selection.style('transform', `scaleX(${scale})`);
-}
-
-function fade(selection, opacity) {
-    selection.style('fill-opacity', opacity);
-}
-
-function setFill(selection, color) {
-    selection.style('fill', color);
-}
+svg.append('rect')
+    .attr('x', width / 2)
+    .attr('width', width / 2)
+    .attr('height', height)
+    .style('fill', 'lightblue')
+    .style('stroke', 'green');
