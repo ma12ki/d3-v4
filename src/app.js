7752b4f821b4f9470a5155017c52ac58540f1ef9
Marcin Zolkiewski <Marcin_Zolkiewski@epam.com>
2016-11-09 23:18:56 +0100

    chart axes

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 const margin = {
     top: 10,
     right: 20,
-    bottom: 25,
-    left: 25
+    bottom: 60,
+    left: 40
 };
 
 const chartWidth = 425;
@@ -18,14 +18,28 @@ const svg = d3.select('.chart')
         .attr('transform', `translate(${margin.left}, ${margin.top})`);
 
 svg.append('rect')
-    .attr('width', width / 2)
+    .attr('width', width)
     .attr('height', height)
     .style('fill', 'lightblue')
     .style('stroke', 'green');
 
-svg.append('rect')
-    .attr('x', width / 2)
-    .attr('width', width / 2)
-    .attr('height', height)
-    .style('fill', 'lightblue')
-    .style('stroke', 'green');
+const yScale = d3.scaleLinear()
+    .domain([0, 100])
+    .range([height, 0]);
+
+const yAxis = d3.axisLeft(yScale).ticks(5);
+svg.call(yAxis);
+
+const xScale = d3.scaleTime()
+    .domain([new Date(2016, 0, 1), new Date(2016, 1, 1)])
+    .range([0, width]);
+
+const xAxis = d3.axisBottom(xScale)
+    .ticks(5)
+    .tickSizeInner(10)
+    .tickSizeOuter(20)
+    .tickPadding(15);
+svg
+    .append('g')
+        .attr('transform', `translate(0, ${height})`)
+    .call(xAxis);
